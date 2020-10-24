1e8cdab9bea41c0d26a4a273cfcf541f93661443
jack155861 <45505414+jack155861@users.noreply.github.com>
2020-10-24 11:09:31 +0800

    Add files via upload

--- combine.py
+++ combine.py
@@ -2,19 +2,24 @@ from PIL import Image
 import numpy as np
 from deeplabv3 import DeepLabModel
 from trimap import trimap
-import sys, os
+import sys,os
 sys.path.insert(1, 'FBA_Matting')
 from networks.models import build_model
 from demo import pred
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-MODEL = DeepLabModel("deeplabv3_pascal_trainval_2018_01_04.tar.gz")
 
-class Args:
-    encoder = 'resnet50_GN_WS'
-    decoder = 'fba_decoder'
-    weights = 'FBA_Matting/FBA.pth'
-args=Args()
-matting_model = build_model(args)
+
+def deeplabv3_model():
+    return DeepLabModel("deeplabv3_pascal_trainval_2018_01_04.tar.gz")
+
+
+def FBA_model():
+    class Args:
+        encoder = 'resnet50_GN_WS'
+        decoder = 'fba_decoder'
+        weights = 'FBA_Matting/FBA.pth'
+    args=Args()
+    matting_model = build_model(args)
+    return matting_model
 
 def read_image(name):
     return (np.array(name) / 255.0)
@@ -26,9 +31,9 @@ def read_trimap(name):
     trimap[trimap_im == 0, 0] = 1
     return trimap
 
-def auto_hair(image_url, out_, in_):
+def auto_hair(image_url, out_, in_, MODEL1, model2):
     original_im = Image.open(image_url)
-    resized_im, seg_map = MODEL.run(original_im)
+    resized_im, seg_map = MODEL1.run(original_im)
     mask = np.equal(seg_map, 15)
     mask = mask.astype(int)
     mask = mask*255
@@ -38,7 +43,7 @@ def auto_hair(image_url, out_, in_):
     trimap_im = Image.fromarray(trimap(np.array(mask), out_, in_))
     input_resized_im = read_image(resized_im)
     input_trimap_im = read_trimap(trimap_im)
-    fg, bg, alpha = pred(input_resized_im, input_trimap_im, matting_model)
+    fg, bg, alpha = pred(input_resized_im, input_trimap_im, model2)
     alpha = Image.fromarray((alpha*255).astype(np.uint8))
     final = Image.fromarray((input_resized_im*255).astype(np.uint8))
     final.putalpha(alpha)
